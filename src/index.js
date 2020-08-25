72cc165c0d015946bfcaeac1a7c1ab52244c11a9
Dhrumil <dhrumilgandhi540@gmail.com>
2020-08-26 01:27:55 +0530

    test to modified index.jd

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import "./index.css";
 import App from './App';
 
 ReactDOM.render(<App /> , document.getElementById('root'));
+
+const sGame =["call of duty","far city","PUBG"];
+const rGame =['Aspal8', 'Need For Speed'];
+
+var game =[...sGame,...rGame];
+console.log(game);
