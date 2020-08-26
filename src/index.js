4ff9fa23be0a970129f039aaaca077d80f645015
Dhrumil <dhrumilgandhi540@gmail.com>
2020-08-26 16:22:11 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ import "./index.css";
 import App from './App';
 
 ReactDOM.render(<App /> , document.getElementById('root'));
-
-const sGame =["call of duty","far city","PUBG"];
-const rGame =['Aspal8', 'Need For Speed'];
-
-var game =[...sGame,...rGame];
-console.log(game);
