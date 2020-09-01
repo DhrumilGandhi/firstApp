8b3e0cb2e95dd09e5603239fadbe53e6b8e36374
Dhrumil <dhrumilgandhi540@gmail.com>
2020-09-02 01:08:24 +0530

    inc-dec pro

--- src/App.js
+++ src/App.js
@@ -1,59 +1,29 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [fullName, setFullName] = useState({
-    fname:'',
-    lname:'',
-    email:'',
-    phone:'',
-  });
-  const inputEvent = (event) => {
-    const {name, value} = event.target;
-    setFullName((preValue) =>{
-      return{
-        ...preValue,
-        [name] : value,}
-    })
+  let [inc, setInc] = useState(0);
 
+  let a = () => {
+    setInc(inc + 1);
   }
-  const clickChange = (event) => {
-    event.preventDefault();
-    alert('Data Submited');
+  let b = () => {
+    if (inc <= 0) {
+      alert("O thi niche nai jay bhai");
+      inc = 0;
+    }
+    else {
+      setInc(inc - 1);
+    }
   }
   return (
     <>
-      <form onSubmit={clickChange}>
-        <div>
-          <h1>Hello {fullName.fname} {fullName.lname}</h1>
-          <p> {fullName.email}</p>
-          <p> {fullName.phone}</p>
-          <input type='text'
-            placeholder='Enter your Name'
-            name='fname'
-            onChange={inputEvent}
-            defaultValue={fullName.fname}
-          /><br />
-          <input type='text'
-            placeholder='Enter your Name Last Name'
-            name='lname'
-            onChange={inputEvent}
-            defaultValue={fullName.lname}
-          /><br/>
-          <input type='email'
-            placeholder='Enter your Email ID'
-            name='email'
-            onChange={inputEvent}
-            defaultValue={fullName.email}
-          /><br/>
-          <input type='number'
-            placeholder='Enter your Phone Number'
-            name='phone'
-            onChange={inputEvent}
-            defaultValue={fullName.phone}
-          /><br/>
-          <button type='submit'>Submit</button>
+      <div className='main_div'>
+        <div className='center_div'>
+          <h1>{inc}</h1><br />
+          <button onClick={a} >Increment</button>
+          <button onClick={b} >Decrement</button>
         </div>
-      </form>
+      </div>
     </>
   )
 }
