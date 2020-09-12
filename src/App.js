447ce2df6b76093986fbc9b50ee1cb411d0979d7
Dhrumil <dhrumilgandhi540@gmail.com>
2020-09-13 02:38:06 +0530

    add use Effect method and remove all css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const App = () => {
-  const [inc, setInc] = useState(0);
+  const [num, setNum] = useState(0);
+  const click=() =>{
+    setNum(num+1)
+    
+  }
+  useEffect(()=>{
+    document.title = `you click me ${num} time`;
+  },[num])
 
-  const a = () => {
-    setInc(inc + 1);
-  }
-  const b = () => {
-    if (inc <= 0) {
-      alert("O thi niche nai jay bhai");
-      setInc(0);
-    }
-    else {
-      setInc(inc - 1);
-    }
-  }
   return (
     <>
-      <div className='main_div'>
-        <div className='center_div'>
-          <h1>{inc}</h1><br />
-          <button onClick={a} >Increment</button>
-          <button onClick={b} >Decrement</button>
-        </div>
-      </div>
+    <button onClick={click} >click me {num}</button>
+      {/* <h1>This is App</h1> */}
     </>
   )
 }
+
 export default App;
