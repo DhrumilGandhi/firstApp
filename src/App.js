219e08800184aaad11fb044637a50370d8ac86fc
Dhrumil <dhrumilgandhi540@gmail.com>
2020-09-15 00:48:20 +0530

    add react router-dom

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import {Route , Switch } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import Contact from './Contact';
 
-const App = () => {
-  const [num, setNum] = useState(1);
-  const [name, setName] = useState();
-  const [moves, setMoves] = useState();
-  useEffect(()=>{
-    async function getData(){
-      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
-      setName(res.data.name);
-      setMoves(res.data.moves.length);
-    }
-    getData();
-  })
-  return (
+
+const App = () =>{
+  return(
     <>
-      <h1>You choose <span style = { {color:'red'}}>{num}</span> value </h1>
-      <h1>My name is  <span style = { {color:'red'}}>{name}</span> value</h1>
-      <h1>I have  <span style = { {color:'red'}}>{moves}</span> moves</h1>
-      <select value={num} onChange={(event) => {
-        setNum(event.target.value);
-      }}>
-        <option value='1'>1</option>
-        <option value='25'>25</option>
-        <option value='3'>3</option>
-        <option value='4'>4</option>
-        <option value='5'>5</option>
-      </select>
+      <Switch>
+        <Route exact path='/' component = {Home} />
+        <Route path='/about' component = {About} />
+        <Route path='/contact' component = {Contact} />
+      </Switch>
     </>
   )
 }
